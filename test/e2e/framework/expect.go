e6b815344546f83499e6e9df3a85f0721a69b2ea
Ivan Shvedunov <ivan.shvedunov@travelping.com>
2021-03-22 17:11:06 +0300

    Add missing copyright notices and LICENSE file
    Add copyright notices to E2E tests
    Fixes #103 (missing LICENSE file)

--- test/e2e/framework/expect.go
+++ test/e2e/framework/expect.go
@@ -1,3 +1,19 @@
+// expect.go - 3GPP TS 29.244 GTP-U UP plug-in
+//
+// Copyright (c) 2021 Travelping GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /*
 Derived from https://github.com/kubernetes/kubernetes/blob/84096f02e9ecb1dd596d3e05b56238485e4ba051/test/e2e/framework/expect.go
 
