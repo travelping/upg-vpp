df03b2b3f2f8ae2438ef6da09cacc978d79a258f
Sergey Matov <sergey.matov@travelping.com>
2021-05-13 16:09:52 +0400

    BBF PFCP NAT functionality

--- upf/test/test_upf.py
+++ upf/test/test_upf.py
@@ -100,7 +100,9 @@ class IPv4Mixin(object):
             "upf application TST rule 3001 add ipfilter " +
             "permit out ip from %s to assigned" % APP_RULE_IP_V4,
             "nat44 enable sessions 1024 endpoint-dependent",
-            "upf nat pool 78.32.0.2 - 78.32.0.25 block_size 512 vrf 200 name testing",
+            "upf nat pool nwi sgi 78.32.0.2 - 78.32.0.25 block_size 512 vrf 200 name testing",
+            "upf nat pool nwi sgi 78.32.20.2 - 78.32.20.25 block_size 512 vrf 200 name not-testing",
+            "upf ueip pool nwi sgi id mypool",
         ]
 
     @classmethod
@@ -117,7 +119,9 @@ class IPv4Mixin(object):
             "upf gtpu endpoint ip %s nwi cp teid 0x80000000/2" % cls.if_cp.local_ip4,
             "upf gtpu endpoint ip %s nwi epc teid 0x80000000/2" % cls.if_grx.local_ip4,
             "nat44 enable sessions 1024 endpoint-dependent",
-            "upf nat pool 78.32.0.2 - 78.32.0.250 block_size 512 vrf 200 name testing",
+            "upf nat pool nwi sgi 78.32.0.2 - 78.32.0.250 block_size 512 vrf 200 name testing",
+            "upf nat pool nwi sgi 78.32.20.2 - 78.32.20.25 block_size 512 vrf 200 name not-testing",
+            "upf ueip pool nwi sgi id mypool",
         ]
 
     @property
@@ -363,7 +367,6 @@ class PFCPHelper(object):
             IE_NodeId(id_type="FQDN", id="ergw")
         ]), PFCPAssociationSetupResponse)
         self.assertEqual(CauseValues[resp[IE_Cause].cause], "Request accepted")
-        self.assertIn(b"vpp", resp[IE_EnterpriseSpecific].data)
         if IE_NodeId in resp:
             if resp[IE_NodeId].id_type is 2:
                 self.assertEqual(resp[IE_NodeId].id, b"upg")
