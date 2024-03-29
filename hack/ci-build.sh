d15df5adde8703dba8200b597f18eea4f2385e1d
Marcin Żyła <marcin.zyla@vprocess.pl>
2024-03-29 11:10:28 +0100

    ci: switch to github hosted runners (#398)

--- hack/ci-build.sh
+++ hack/ci-build.sh
@@ -19,17 +19,14 @@ function do_build {
   # --export-cache type=inline \
   # --import-cache type=registry,ref="${IMAGE_BASE_NAME}" \
   set -x
-  buildctl build \
-           --frontend dockerfile.v0 \
-           --progress=plain \
-           --local context=. \
-           --local dockerfile=. \
-           --opt filename="${DOCKERFILE}" \
-           --opt build-arg:BUILD_TYPE=${BUILD_TYPE} \
-           --opt build-arg:BASE=${VPP_IMAGE_BASE}_${BUILD_TYPE} \
-           --opt build-arg:DEVBASE=${VPP_IMAGE_BASE}_dev_${BUILD_TYPE} \
-           --opt label:quay.expires-after="${IMAGE_EXPIRES_AFTER}" \
-           "$@"
+  docker buildx build \
+    --progress plain \
+    --file "${DOCKERFILE}" \
+    --build-arg BUILD_TYPE=${BUILD_TYPE} \
+    --build-arg BASE=${VPP_IMAGE_BASE}_${BUILD_TYPE} \
+    --build-arg DEVBASE=${VPP_IMAGE_BASE}_dev_${BUILD_TYPE} \
+    --label "quay.expires-after=${IMAGE_EXPIRES_AFTER}" \
+    . "$@"
    set +x
 }
 
@@ -42,18 +39,18 @@ FINAL_IMAGE_NAME="${IMAGE_BASE_NAME}:${IMAGE_BASE_TAG}_${BUILD_TYPE}"
 echo >&2 "Building VPP and extracting the artifacts ..."
 rm -rf /tmp/_out
 mkdir /tmp/_out
-do_build --opt target=artifacts --output type=local,dest=/tmp/_out
+do_build --target=artifacts --output type=local,dest=/tmp/_out
 
 echo >&2 "Building the dev image from ${DOCKERFILE} ..."
 push=",push=true"
 if [[ ${NO_PUSH} ]]; then
   push=""
 fi
-do_build --opt target=dev-stage \
+do_build --target=dev-stage \
          --output type="image,\"name=${DEV_IMAGE_NAME}\"${push}"
 
 echo >&2 "Building the final image from ${DOCKERFILE} ..."
-do_build --opt target=final-stage \
+do_build --target=final-stage \
          --output type="image,\"name=${FINAL_IMAGE_NAME}\"${push}"
 
 echo "${DEV_IMAGE_NAME}" > "image-dev-${BUILD_TYPE}.txt"
